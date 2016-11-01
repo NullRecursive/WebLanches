d72db1f55a66f23f96b193367f6df28921454f00
brunowall <brunowallison.fernandes@gmail.com>
2016-10-31 23:39:13 -0300

    add the login and register form

--- loja/views.py
+++ loja/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
 	return render(request, 'loja/base.html')
+
+def login_page(request):
+	return render(request,'loja/login.html')
+
+def cad_page(request):
+	return render(request,'loja/cadastro.html')
