b22b379eea36beb388284f8ce25b7b134a2adb58
brunowall <brunowallison.fernandes@gmail.com>
2016-11-03 02:32:10 -0300

    adicionado modl user

---
+++ loja/migrations/0001_initial.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.contrib.auth.models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('auth', '0006_require_contenttypes_0002'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Usuario',
+            fields=[
+                ('user_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, to=settings.AUTH_USER_MODEL, serialize=False)),
+                ('telefone', models.CharField(max_length=20)),
+                ('cpf', models.CharField(max_length=20)),
+            ],
+            options={
+                'verbose_name': 'user',
+                'verbose_name_plural': 'users',
+                'abstract': False,
+            },
+            bases=('auth.user',),
+            managers=[
+                ('objects', django.contrib.auth.models.UserManager()),
+            ],
+        ),
+    ]
