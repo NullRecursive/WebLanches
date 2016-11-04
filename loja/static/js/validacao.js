b61bc2cf38a1315f862361db42c6265d9fc94f63
Gabriel Cesar <gcal1@live.com>
2016-11-04 14:49:35 -0300

    validacao de senha corrigido

--- loja/static/js/validacao.js
+++ loja/static/js/validacao.js
@@ -1,13 +1,7 @@
-var senha = document.getElementById("senha"),
-	conf_senha = document.getElementById("csenha");
 
-function validatePassword(){
-  if(senha.value != conf_senha.value) {
-    conf_senha.setCustomValidity("As senhas são diferentes!");
-  } else {
-    conf_senha.setCustomValidity('');
-  }
+function validarSenha(senha, conf_senha){	
+  	if(senha.value != conf_senha.value) 
+  		conf_senha.setCustomValidity("As senhas são diferentes!");
+  	else
+  		conf_senha.setCustomValidity('');
 }
-
-senha.onchange = validatePassword;
-conf_senha.onkeyup = validatePassword;
