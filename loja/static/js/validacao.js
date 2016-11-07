0c7f626c1935fdb0503b0b4ff59ed8ffb9c86deb
Gabriel Cesar <gcal1@live.com>
2016-11-06 22:47:07 -0300

    update

--- loja/static/js/validacao.js
+++ loja/static/js/validacao.js
@@ -14,12 +14,3 @@ function validarSenha(senha, conf_senha){
   		conf_senha.setCustomValidity('');
 }
 
-//Comandos JQuery
-// Ainda nao esta funcionando
-
-jQuery(function($){
-   $("cep").mask("xxxxx-xxx");
-//   $("#phone").mask("(999) 999-9999");
-  //  $("#tin").mask("99-9999999");
-   // 	$("#ssn").mask("999-99-9999");
-});
