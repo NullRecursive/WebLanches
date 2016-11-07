1b0d1bd3bdd774308bfaab3817d5b0394ca4cbf3
Gabriel Cesar <gcal1@live.com>
2016-11-06 22:02:49 -0300

    teste

--- loja/static/js/validacao.js
+++ loja/static/js/validacao.js
@@ -1,4 +1,5 @@
-	
+// Comandos JavaScript
+
 function validarTamSenha(senha) {
 	if (senha.value.length < 4)
 		senha.setCustomValidity("A senha deve ter no minimo 4 caracteres!");
@@ -7,9 +8,18 @@ function validarTamSenha(senha) {
 }
 
 function validarSenha(senha, conf_senha){	
-  	if(senha.value != conf_senha.value) {
+  	if(senha.value != conf_senha.value) 
   		conf_senha.setCustomValidity("As senhas são diferentes!");
-  	}
   	else
   		conf_senha.setCustomValidity('');
 }
+
+//Comandos JQuery
+// Ainda nao esta funcionando
+
+jQuery(function($){
+   $("cep").mask("xxxxx-xxx");
+//   $("#phone").mask("(999) 999-9999");
+  //  $("#tin").mask("99-9999999");
+   // 	$("#ssn").mask("999-99-9999");
+});
