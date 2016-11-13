37d473c0a82764cd0cca7cda22d16d229386bba9
Gabriel Cesar <gcal1@live.com>
2016-11-13 00:40:05 -0300

    corrigindo erro de javascript

--- loja/urls.py
+++ loja/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home_page),
-    url(r'^entrar/$', views.login_page, name='login'),
+    url(r'^entrar/$', views.login_page, name ='login'),
     url(r'^cadastrar/$', views.cad_page, name = 'cadastrar'),
     url(r'^cardapio/$', views.cardapio, name = 'cardapio'),
 ]
