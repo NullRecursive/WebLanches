42a28d27469fa27e4d1f8bb92e2b11c78cc80167
Gabriel Cesar <gcal1@live.com>
2016-11-16 00:48:25 -0300

    update urls

--- loja/urls.py
+++ loja/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^home/^$', views.home, name = 'home'),
+    url(r'^$', views.home, name = 'home'),
     url(r'^entrar/$', views.login_page, name = 'login'),
     url(r'^cadastrar/$', views.cad_page, name = 'cadastrar'),
     url(r'^cardapio/$', views.cardapio, name = 'cardapio'),
