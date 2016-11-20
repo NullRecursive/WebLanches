54df65f84646eec6e567d615571fdffdc4d643ef
Gabriel Cesar <gcal1@live.com>
2016-11-20 00:58:51 -0300

    modified:   loja/models.py 	modified:   loja/static/css/loja.css 	modified:   loja/templates/loja/base.html

--- loja/models.py
+++ loja/models.py
@@ -3,10 +3,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Usuario(User):
-	telefone = models.CharField(max_length=20)
-	cpf = models.CharField(max_length=20)
-	endereco = models.CharField(max_length=250)
-	cep = models.CharField(max_length=8)
+	telefone = models.CharField(max_length = 20)
+	cpf = models.CharField(max_length = 20)
+	endereco = models.CharField(max_length = 250)
+	cep = models.CharField(max_length = 8)
 	
 	def save(self,*args,**kwargs):
 		self.set_password(self.password)
@@ -16,7 +16,7 @@ class Produto(models.Model):
 	nome = models.CharField(max_length = 250, primary_key = True)
 	preco = models.FloatField()
 	descricao = models.TextField(blank = True)
-	imagem = models.ImageField(upload_to='loja/static/product_images') #falta corrigir bug no GET
+	imagem = models.ImageField(upload_to = 'loja/static/product_images') #falta corrigir bug no GET
 	em_Falta = models.BooleanField()
 
 	def __str__(self):
@@ -30,8 +30,11 @@ class Item(models.Model):
 	quantidade = models.IntegerField(default = 0)
 
 class Pedido(models.Model):
-	meus_itens = models.TextField(null=True) #JSON-serialized (text) 
-	usuario=models.ForeignKey(
+	meus_itens = models.TextField(null = True) #JSON-serialized (text) 
+	usuario = models.ForeignKey(
 		'Usuario',
 		on_delete = models.CASCADE,
 	)
+
+	def calcularTotal():
+		pass
