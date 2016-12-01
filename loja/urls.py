f469a9ce02cee56cf065355c6b39cbe43f2a4d8c
Patrick <patrickeeep12@gmail.com>
2016-12-01 16:39:53 -0300

    loja home.html base.html urls.py

--- loja/urls.py
+++ loja/urls.py
@@ -3,10 +3,9 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^$', views.home, name = 'home'),
+    url(r'^$', views.home, name = 'cardapio'),
+    url(r'^cardapio/hamburguer/$', views.hamburguer, name = 'hamburguer'),
     url(r'^entrar/$', views.login_page, name = 'login'),
     url(r'^cadastrar/$', views.cad_page, name = 'cadastrar'),
-    url(r'^cardapio/$', views.cardapio, name = 'cardapio'),
-    url(r'^cardapio/hamburguer/$', views.hamburguer, name = 'hamburguer'),
     url(r'^sair$', views.sair, name = 'sair'),
 ]
