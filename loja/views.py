531d92ecbb712be47afec0497984213f25d1bc6d
Gabriel Cesar <gcal1@live.com>
2016-12-04 02:23:49 -0300

    modified:   loja/views.py

--- loja/views.py
+++ loja/views.py
@@ -5,24 +5,24 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from .controllers import ControllerUsuario
 
+
 def home(request):
 	return redirect(cardapio)
-
-def cardapio(request):
-	return redirect(hamburguer)
 
 def login_page(request):
 	if request.method == 'POST':
 		form = FormLogin(request.POST)
 		controller =  ControllerUsuario()
 		if controller.logar(request,form):
-			return redirect(cadapio)
+			return redirect(cardapio)
 		
 	else:
 		form = FormLogin()
 
 	return render(request,'loja/login.html',{'form': form})
 
+def cardapio(request):
+	return redirect(hamburguer)
 
 def cad_page(request):
 	if request.method == 'POST':
