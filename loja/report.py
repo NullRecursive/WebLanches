7a9afaf77b20e90b741a1a38619cffcfd0d0365b
Gabriel Cesar <gcal1@live.com>
2016-12-14 21:32:04 -0300

    modified:   loja/report.py 	modified:   loja/templates/loja/pedido/template_comprovante.html

--- loja/report.py
+++ loja/report.py
@@ -1,18 +1,19 @@
 # comentar se nao funcionar 
 from xhtml2pdf import pisa 
-from io import StringIO
+from io import StringIO, BytesIO
 from django.template.loader import get_template 
 from django.template import Context 
 from django.http import HttpResponse
 
-def write_to_pdf(request,template_path, context): 
+def write_to_pdf(request, template_path, context): 
     template = get_template(template_path) 
     context = Context(context) 
     html = template.render(context) 
-    result = StringIO.StringIO() 
-    pdf = pisa.pisaDocument(StringIO.StringIO(html),dest=result) 
-    if not pdf.err: 
-        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    result = BytesIO()
+    pdf = pisa.pisaDocument(StringIO(html), dest = result)
 
-    else: return HttpResponse('Errors')
+    if not pdf.err: 
+        return HttpResponse(result.getvalue(), content_type = 'application/pdf')
+    else: 
+        return HttpResponse('Não foi possivel gerar a impressao do comprovante')
     
